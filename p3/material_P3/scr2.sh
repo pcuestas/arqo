98d1673f6d8a28a166cb6d7763ffc0dc94973c9b
pcuestas <pablocsaa@gmail.com>
2021-11-09 12:27:56 +0100

    reorganización

--- p3/material_P3/scr2.sh
+++ p3/material_P3/scr2.sh
@@ -56,11 +56,11 @@ set term png
 set output "out2/cache_lectura.png"
 plot "out2/1024.dat" using 1:2 with lines lw 2 title "D1mr slow - Level 1: 1024B", \
      "out2/1024.dat" using 1:4 with lines lw 2 title "D1mr fast - Level 1: 1024B", \
-	"out2/2048.dat" using 1:2 with lines lw 2 title "D1mr slow - Level 1: 2048B", \
+		 "out2/2048.dat" using 1:2 with lines lw 2 title "D1mr slow - Level 1: 2048B", \
      "out2/2048.dat" using 1:4 with lines lw 2 title "D1mr fast - Level 1: 2048B", \
-	"out2/4096.dat" using 1:2 with lines lw 2 title "D1mr slow - Level 1: 4096B", \
+		 "out2/4096.dat" using 1:2 with lines lw 2 title "D1mr slow - Level 1: 4096B", \
      "out2/4096.dat" using 1:4 with lines lw 2 title "D1mr fast - Level 1: 4096B", \
-	"out2/8192.dat" using 1:2 with lines lw 2 title "D1mr slow - Level 1: 8192B", \
+		 "out2/8192.dat" using 1:2 with lines lw 2 title "D1mr slow - Level 1: 8192B", \
      "out2/8192.dat" using 1:4 with lines lw 2 title "D1mr fast - Level 1: 8192B"
 replot
 
@@ -73,11 +73,11 @@ set term png
 set output "out2/cache_escritura.png"
 plot "out2/1024.dat" using 1:3 with lines lw 2 title "D1mw slow - Level 1: 1024B", \
      "out2/1024.dat" using 1:5 with lines lw 2 title "D1mw fast - Level 1: 1024B", \
-	"out2/2048.dat" using 1:3 with lines lw 2 title "D1mw slow - Level 1: 2048B", \
+		 "out2/2048.dat" using 1:3 with lines lw 2 title "D1mw slow - Level 1: 2048B", \
      "out2/2048.dat" using 1:5 with lines lw 2 title "D1mw fast - Level 1: 2048B", \
-	"out2/4096.dat" using 1:3 with lines lw 2 title "D1mw slow - Level 1: 4096B", \
+		 "out2/4096.dat" using 1:3 with lines lw 2 title "D1mw slow - Level 1: 4096B", \
      "out2/4096.dat" using 1:5 with lines lw 2 title "D1mw fast - Level 1: 4096B", \
-	"out2/8192.dat" using 1:3 with lines lw 2 title "D1mw slow - Level 1: 8192B", \
+		 "out2/8192.dat" using 1:3 with lines lw 2 title "D1mw slow - Level 1: 8192B", \
      "out2/8192.dat" using 1:5 with lines lw 2 title "D1mw fast - Level 1: 8192B"
 replot
 quit
