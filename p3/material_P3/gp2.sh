6792254d8d7b87c0a692221bba86c93afb038b3a
pcuestas <pablocsaa@gmail.com>
2021-11-10 15:34:00 +0100

    Co-authored-by: azamanis <azamanis@users.noreply.github.com>

--- p3/material_P3/gp2.sh
+++ p3/material_P3/gp2.sh
@@ -3,16 +3,16 @@ set ylabel "Number of misses"
 set xlabel "Cache size (B)"
 set key right top
 set grid
-set term png
+set term pngcairo dashed
 set output "out2/mv_att1/cache_lectura.png"
-plot "out2/mv_att1/1024.dat" using 1:2 with lines lt rgb "green"  dt 1 lw 2 title "D1mr slow - Level 1: 1024B", \
-     "out2/mv_att1/1024.dat" using 1:4 with lines lt rgb "green"  dt 2 lw 2 title "D1mr fast - Level 1: 1024B", \
-	"out2/mv_att1/2048.dat" using 1:2 with lines lt rgb "blue"   dt 1 lw 2 title "D1mr slow - Level 1: 2048B", \
-     "out2/mv_att1/2048.dat" using 1:4 with lines lt rgb "blue"   dt 2 lw 2 title "D1mr fast - Level 1: 2048B", \
-	"out2/mv_att1/4096.dat" using 1:2 with lines lt rgb "violet" dt 1 lw 2 title "D1mr slow - Level 1: 4096B", \
-     "out2/mv_att1/4096.dat" using 1:4 with lines lt rgb "violet" dt 2 lw 2 title "D1mr fast - Level 1: 4096B", \
-	"out2/mv_att1/8192.dat" using 1:2 with lines lt rgb "red"    dt 1 lw 2 title "D1mr slow - Level 1: 8192B", \
-     "out2/mv_att1/8192.dat" using 1:4 with lines lt rgb "red"    dt 2 lw 2 title "D1mr fast - Level 1: 8192B"
+plot "out2/mv_att1/1024.dat" using 1:2 with lines lt rgb "#0000FF00"  dt 1 lw 2 title "D1mr slow - Level 1: 1024B", \
+     "out2/mv_att1/1024.dat" using 1:4 with lines lt rgb "#0000FF00"  dt 2 lw 2 title "D1mr fast - Level 1: 1024B", \
+	"out2/mv_att1/2048.dat" using 1:2 with lines lt rgb "#000000FF"   dt 1 lw 2 title "D1mr slow - Level 1: 2048B", \
+     "out2/mv_att1/2048.dat" using 1:4 with lines lt rgb "#000000FF"   dt 2 lw 2 title "D1mr fast - Level 1: 2048B", \
+	"out2/mv_att1/4096.dat" using 1:2 with lines lt rgb "#00800080" dt 1 lw 2 title "D1mr slow - Level 1: 4096B", \
+     "out2/mv_att1/4096.dat" using 1:4 with lines lt rgb "#00800080" dt 2 lw 2 title "D1mr fast - Level 1: 4096B", \
+	"out2/mv_att1/8192.dat" using 1:2 with lines lt rgb "#00FF0000"    dt 1 lw 2 title "D1mr slow - Level 1: 8192B", \
+     "out2/mv_att1/8192.dat" using 1:4 with lines lt rgb "#00FF0000"    dt 2 lw 2 title "D1mr fast - Level 1: 8192B"
 replot
 
 set title "Cache write misses"
@@ -20,15 +20,15 @@ set ylabel "Number of misses"
 set xlabel "Matrix size (N)"
 set key right top
 set grid
-set term png
+set term pngcairo dashed
 set output "out2/mv_att1/cache_escritura.png"
-plot "out2/mv_att1/1024.dat" using 1:3 with lines lt rgb "green"  dt 1 lw 2 title "D1mw slow - Level 1: 1024B", \
-     "out2/mv_att1/1024.dat" using 1:5 with lines lt rgb "green"  dt 2 lw 2 title "D1mw fast - Level 1: 1024B", \
-	"out2/mv_att1/2048.dat" using 1:3 with lines lt rgb "blue"   dt 1 lw 2 title "D1mw slow - Level 1: 2048B", \
-     "out2/mv_att1/2048.dat" using 1:5 with lines lt rgb "blue"   dt 2 lw 2 title "D1mw fast - Level 1: 2048B", \
-	"out2/mv_att1/4096.dat" using 1:3 with lines lt rgb "violet" dt 1 lw 2 title "D1mw slow - Level 1: 4096B", \
-     "out2/mv_att1/4096.dat" using 1:5 with lines lt rgb "violet" dt 2 lw 2 title "D1mw fast - Level 1: 4096B", \
-	"out2/mv_att1/8192.dat" using 1:3 with lines lt rgb "red"    dt 1 lw 2 title "D1mw slow - Level 1: 8192B", \
-     "out2/mv_att1/8192.dat" using 1:5 with lines lt rgb "red"    dt 2 lw 2 title "D1mw fast - Level 1: 8192B"
+plot "out2/mv_att1/1024.dat" using 1:3 with lines lt rgb "#9000FF00"  dt 1 lw 2 title "D1mw slow - Level 1: 1024B", \
+     "out2/mv_att1/1024.dat" using 1:5 with lines lt rgb "#0000FF00"  dt 2 lw 2 title "D1mw fast - Level 1: 1024B", \
+	"out2/mv_att1/2048.dat" using 1:3 with lines lt rgb "#900000FF"   dt 1 lw 2 title "D1mw slow - Level 1: 2048B", \
+     "out2/mv_att1/2048.dat" using 1:5 with lines lt rgb "#000000FF"   dt 2 lw 2 title "D1mw fast - Level 1: 2048B", \
+	"out2/mv_att1/4096.dat" using 1:3 with lines lt rgb "#90800080" dt 1 lw 2 title "D1mw slow - Level 1: 4096B", \
+     "out2/mv_att1/4096.dat" using 1:5 with lines lt rgb "#00800080" dt 2 lw 2 title "D1mw fast - Level 1: 4096B", \
+	"out2/mv_att1/8192.dat" using 1:3 with lines lt rgb "#90FF0000"    dt 1 lw 2 title "D1mw slow - Level 1: 8192B", \
+     "out2/mv_att1/8192.dat" using 1:5 with lines lt rgb "#00FF0000"    dt 2 lw 2 title "D1mw fast - Level 1: 8192B"
 replot
 quit
--- p3/material_P3/scr2.sh
+++ p3/material_P3/scr2.sh
@@ -52,16 +52,16 @@ set ylabel "Number of misses"
 set xlabel "Cache size (B)"
 set key right top
 set grid
-set term png
-set output "out2/cache_lectura.png"
-plot "out2/1024.dat" using 1:2 with lines lw 2 title "D1mr slow - Level 1: 1024B", \
-     "out2/1024.dat" using 1:4 with lines lw 2 title "D1mr fast - Level 1: 1024B", \
-		 "out2/2048.dat" using 1:2 with lines lw 2 title "D1mr slow - Level 1: 2048B", \
-     "out2/2048.dat" using 1:4 with lines lw 2 title "D1mr fast - Level 1: 2048B", \
-		 "out2/4096.dat" using 1:2 with lines lw 2 title "D1mr slow - Level 1: 4096B", \
-     "out2/4096.dat" using 1:4 with lines lw 2 title "D1mr fast - Level 1: 4096B", \
-		 "out2/8192.dat" using 1:2 with lines lw 2 title "D1mr slow - Level 1: 8192B", \
-     "out2/8192.dat" using 1:4 with lines lw 2 title "D1mr fast - Level 1: 8192B"
+set term pngcairo dashed
+set output "$fPNGread"
+plot "out2/1024.dat" using 1:2 with lines lt rgb "#0000FF00"  dt 1 lw 2 title "D1mr slow - Level 1: 1024B", \
+  "out2/1024.dat" using 1:4 with lines lt rgb "#0000FF00"  dt 2 lw 2 title "D1mr fast - Level 1: 1024B", \
+	"out2/2048.dat" using 1:2 with lines lt rgb "#000000FF"   dt 1 lw 2 title "D1mr slow - Level 1: 2048B", \
+  "out2/2048.dat" using 1:4 with lines lt rgb "#000000FF"   dt 2 lw 2 title "D1mr fast - Level 1: 2048B", \
+	"out2/4096.dat" using 1:2 with lines lt rgb "#00800080" dt 1 lw 2 title "D1mr slow - Level 1: 4096B", \
+  "out2/4096.dat" using 1:4 with lines lt rgb "#00800080" dt 2 lw 2 title "D1mr fast - Level 1: 4096B", \
+	"out2/8192.dat" using 1:2 with lines lt rgb "#00FF0000"    dt 1 lw 2 title "D1mr slow - Level 1: 8192B", \
+  "out2/8192.dat" using 1:4 with lines lt rgb "#00FF0000"    dt 2 lw 2 title "D1mr fast - Level 1: 8192B"
 replot
 
 set title "Cache write misses"
@@ -69,16 +69,16 @@ set ylabel "Number of misses"
 set xlabel "Matrix size (N)"
 set key right top
 set grid
-set term png
-set output "out2/cache_escritura.png"
-plot "out2/1024.dat" using 1:3 with lines lw 2 title "D1mw slow - Level 1: 1024B", \
-     "out2/1024.dat" using 1:5 with lines lw 2 title "D1mw fast - Level 1: 1024B", \
-		 "out2/2048.dat" using 1:3 with lines lw 2 title "D1mw slow - Level 1: 2048B", \
-     "out2/2048.dat" using 1:5 with lines lw 2 title "D1mw fast - Level 1: 2048B", \
-		 "out2/4096.dat" using 1:3 with lines lw 2 title "D1mw slow - Level 1: 4096B", \
-     "out2/4096.dat" using 1:5 with lines lw 2 title "D1mw fast - Level 1: 4096B", \
-		 "out2/8192.dat" using 1:3 with lines lw 2 title "D1mw slow - Level 1: 8192B", \
-     "out2/8192.dat" using 1:5 with lines lw 2 title "D1mw fast - Level 1: 8192B"
+set term pngcairo dashed
+set output "$fPNGwrite"
+plot "out2/1024.dat" using 1:3 with lines lt rgb "#9000FF00"  dt 1 lw 2 title "D1mw slow - Level 1: 1024B", \
+  "out2/1024.dat" using 1:5 with lines lt rgb "#0000FF00"  dt 2 lw 2 title "D1mw fast - Level 1: 1024B", \
+	"out2/2048.dat" using 1:3 with lines lt rgb "#900000FF"   dt 1 lw 2 title "D1mw slow - Level 1: 2048B", \
+  "out2/2048.dat" using 1:5 with lines lt rgb "#000000FF"   dt 2 lw 2 title "D1mw fast - Level 1: 2048B", \
+	"out2/4096.dat" using 1:3 with lines lt rgb "#90800080" dt 1 lw 2 title "D1mw slow - Level 1: 4096B", \
+  "out2/4096.dat" using 1:5 with lines lt rgb "#00800080" dt 2 lw 2 title "D1mw fast - Level 1: 4096B", \
+	"out2/8192.dat" using 1:3 with lines lt rgb "#90FF0000"    dt 1 lw 2 title "D1mw slow - Level 1: 8192B", \
+  "out2/8192.dat" using 1:5 with lines lt rgb "#00FF0000"    dt 2 lw 2 title "D1mw fast - Level 1: 8192B"
 replot
 quit
 END_GNUPLOT
