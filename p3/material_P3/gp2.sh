472940c033d9f60eedd673c567fb524cc89f1a45
pcuestas <pablocsaa@gmail.com>
2021-11-10 13:56:13 +0100

    miercoles

--- p3/material_P3/gp2.sh
+++ p3/material_P3/gp2.sh
@@ -5,14 +5,14 @@ set key right top
 set grid
 set term png
 set output "out2/mv_att1/cache_lectura.png"
-plot "out2/mv_att1/1024.dat" using 1:2 with lines lw 2 title "D1mr slow - Level 1: 1024B", \
-     "out2/mv_att1/1024.dat" using 1:4 with lines lw 2 title "D1mr fast - Level 1: 1024B", \
-	"out2/mv_att1/2048.dat" using 1:2 with lines lw 2 title "D1mr slow - Level 1: 2048B", \
-     "out2/mv_att1/2048.dat" using 1:4 with lines lw 2 title "D1mr fast - Level 1: 2048B", \
-	"out2/mv_att1/4096.dat" using 1:2 with lines lw 2 title "D1mr slow - Level 1: 4096B", \
-     "out2/mv_att1/4096.dat" using 1:4 with lines lw 2 title "D1mr fast - Level 1: 4096B", \
-	"out2/mv_att1/8192.dat" using 1:2 with lines lw 2 title "D1mr slow - Level 1: 8192B", \
-     "out2/mv_att1/8192.dat" using 1:4 with lines lw 2 title "D1mr fast - Level 1: 8192B"
+plot "out2/mv_att1/1024.dat" using 1:2 with lines lt rgb "green"  dt 1 lw 2 title "D1mr slow - Level 1: 1024B", \
+     "out2/mv_att1/1024.dat" using 1:4 with lines lt rgb "green"  dt 2 lw 2 title "D1mr fast - Level 1: 1024B", \
+	"out2/mv_att1/2048.dat" using 1:2 with lines lt rgb "blue"   dt 1 lw 2 title "D1mr slow - Level 1: 2048B", \
+     "out2/mv_att1/2048.dat" using 1:4 with lines lt rgb "blue"   dt 2 lw 2 title "D1mr fast - Level 1: 2048B", \
+	"out2/mv_att1/4096.dat" using 1:2 with lines lt rgb "violet" dt 1 lw 2 title "D1mr slow - Level 1: 4096B", \
+     "out2/mv_att1/4096.dat" using 1:4 with lines lt rgb "violet" dt 2 lw 2 title "D1mr fast - Level 1: 4096B", \
+	"out2/mv_att1/8192.dat" using 1:2 with lines lt rgb "red"    dt 1 lw 2 title "D1mr slow - Level 1: 8192B", \
+     "out2/mv_att1/8192.dat" using 1:4 with lines lt rgb "red"    dt 2 lw 2 title "D1mr fast - Level 1: 8192B"
 replot
 
 set title "Cache write misses"
@@ -22,13 +22,13 @@ set key right top
 set grid
 set term png
 set output "out2/mv_att1/cache_escritura.png"
-plot "out2/mv_att1/1024.dat" using 1:3 with lines lw 2 title "D1mw slow - Level 1: 1024B", \
-     "out2/mv_att1/1024.dat" using 1:5 with lines lw 2 title "D1mw fast - Level 1: 1024B", \
-	"out2/mv_att1/2048.dat" using 1:3 with lines lw 2 title "D1mw slow - Level 1: 2048B", \
-     "out2/mv_att1/2048.dat" using 1:5 with lines lw 2 title "D1mw fast - Level 1: 2048B", \
-	"out2/mv_att1/4096.dat" using 1:3 with lines lw 2 title "D1mw slow - Level 1: 4096B", \
-     "out2/mv_att1/4096.dat" using 1:5 with lines lw 2 title "D1mw fast - Level 1: 4096B", \
-	"out2/mv_att1/8192.dat" using 1:3 with lines lw 2 title "D1mw slow - Level 1: 8192B", \
-     "out2/mv_att1/8192.dat" using 1:5 with lines lw 2 title "D1mw fast - Level 1: 8192B"
+plot "out2/mv_att1/1024.dat" using 1:3 with lines lt rgb "green"  dt 1 lw 2 title "D1mw slow - Level 1: 1024B", \
+     "out2/mv_att1/1024.dat" using 1:5 with lines lt rgb "green"  dt 2 lw 2 title "D1mw fast - Level 1: 1024B", \
+	"out2/mv_att1/2048.dat" using 1:3 with lines lt rgb "blue"   dt 1 lw 2 title "D1mw slow - Level 1: 2048B", \
+     "out2/mv_att1/2048.dat" using 1:5 with lines lt rgb "blue"   dt 2 lw 2 title "D1mw fast - Level 1: 2048B", \
+	"out2/mv_att1/4096.dat" using 1:3 with lines lt rgb "violet" dt 1 lw 2 title "D1mw slow - Level 1: 4096B", \
+     "out2/mv_att1/4096.dat" using 1:5 with lines lt rgb "violet" dt 2 lw 2 title "D1mw fast - Level 1: 4096B", \
+	"out2/mv_att1/8192.dat" using 1:3 with lines lt rgb "red"    dt 1 lw 2 title "D1mw slow - Level 1: 8192B", \
+     "out2/mv_att1/8192.dat" using 1:5 with lines lt rgb "red"    dt 2 lw 2 title "D1mw fast - Level 1: 8192B"
 replot
 quit
--- p3/material_P3/gp3.sh
+++ p3/material_P3/gp3.sh
@@ -4,9 +4,9 @@ set xlabel "Matrix Size"
 set key left top
 set grid
 set term png
-set output "out3/mult_time.png"
-plot "out3/mult.dat" using 1:2 with lines lw 2 title "Regular multiplication", \
-     "out3/mult.dat" using 1:5 with lines lw 2 title "Transposed multiplication"
+set output "out3/mv_att2/mult_time.png"
+plot "out3/mv_att2/mult.dat" using 1:2 with lines lw 2 title "Regular multiplication", \
+     "out3/mv_att2/mult.dat" using 1:5 with lines lw 2 title "Transposed multiplication"
 replot
 
 set title "Number of misses in multiplication programs"
@@ -15,10 +15,10 @@ set xlabel "Matrix Size"
 set key left top
 set grid
 set term png
-set output "out3/mult_cache.png"
-plot "out3/mult.dat" using 1:3 with lines lw 2 title "D1mr regular multiplication", \
-     "out3/mult.dat" using 1:4 with lines lw 2 title "D1mw regular multiplication", \
-     "out3/mult.dat" using 1:6 with lines lw 2 title "D1mr transposed multiplication", \
-     "out3/mult.dat" using 1:7 with lines lw 2 title "D1mw transposed multiplication"
+set output "out3/mv_att2/mult_cache.png"
+plot "out3/mv_att2/mult.dat" using 1:3 with lines lw 2 title "D1mr regular multiplication", \
+     "out3/mv_att2/mult.dat" using 1:4 with lines lw 2 title "D1mw regular multiplication", \
+     "out3/mv_att2/mult.dat" using 1:6 with lines lw 2 title "D1mr transposed multiplication", \
+     "out3/mv_att2/mult.dat" using 1:7 with lines lw 2 title "D1mw transposed multiplication"
 replot
 quit
