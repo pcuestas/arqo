854010d1969bc8dbc892bc10c72df354f6703935
pcuestas <pablocsaa@gmail.com>
2021-11-16 20:52:28 +0100

    associativity

--- p3/material_P3/protect_out4/asoc/p2/4096/gp4.sh
+++ p3/material_P3/protect_out4/asoc/p2/4096/gp4.sh
@@ -12,8 +12,8 @@ plot "1.dat" using 1:2 with lines lt rgb "#0000FF00"  dt 1 lw 2 title "D1mr regu
      "1.dat" using 1:4 with lines lt rgb "#0000FF00"  dt 2 lw 2 title "D1mr transposed mult. - ways: 1", \
 	"2.dat" using 1:2 with lines lt rgb "#000000FF"   dt 1 lw 2 title "D1mr regular mult. - ways: 2", \
      "2.dat" using 1:4 with lines lt rgb "#000000FF"   dt 2 lw 2 title "D1mr transposed mult. - ways: 2", \
-	"4.dat" using 1:2 with lines lt rgb "#00800080" dt ".-" lw 2 title "D1mr regular mult. - ways: 4", \
-     "4.dat" using 1:4 with lines lt rgb "#00800080" dt 2 lw 2 title "D1mr transposed mult. - ways: 4", \
+	"4.dat" using 1:2 with lines lt rgb "#00800080" dt "-" lw 2 title "D1mr regular mult. - ways: 4", \
+     "4.dat" using 1:4 with lines lt rgb "#00800080" dt "." lw 2 title "D1mr transposed mult. - ways: 4", \
 	"8.dat" using 1:2 with lines lt rgb "#90FF0000"    dt 1 lw 2 title "D1mr regular mult. - ways: 8", \
      "8.dat" using 1:4 with lines lt rgb "#00FF0000"    dt 2 lw 2 title "D1mr transposed mult. - ways: 8"
 replot
@@ -29,8 +29,8 @@ plot "1.dat" using 1:3 with lines lt rgb "#9000FF00"  dt 1 lw 2 title "D1mw regu
      "1.dat" using 1:5 with lines lt rgb "#0000FF00"  dt 2 lw 2 title "D1mw transposed mult. - ways: 1", \
 	"2.dat" using 1:3 with lines lt rgb "#900000FF"   dt 1 lw 2 title "D1mw regular mult. - ways: 2", \
      "2.dat" using 1:5 with lines lt rgb "#000000FF"   dt 2 lw 2 title "D1mw transposed mult. - ways: 2", \
-	"4.dat" using 1:3 with lines lt rgb "#00800080" dt ".-" lw 2 title "D1mw regular mult. - ways: 4", \
-     "4.dat" using 1:5 with lines lt rgb "#00800080" dt 2 lw 2 title "D1mw transposed mult. - ways: 4", \
+	"4.dat" using 1:3 with lines lt rgb "#00800080" dt "-" lw 2 title "D1mw regular mult. - ways: 4", \
+     "4.dat" using 1:5 with lines lt rgb "#00800080" dt "." lw 2 title "D1mw transposed mult. - ways: 4", \
 	"8.dat" using 1:3 with lines lt rgb "#90FF0000"    dt 1 lw 2 title "D1mw regular mult. - ways: 8", \
      "8.dat" using 1:5 with lines lt rgb "#00FF0000"    dt 2 lw 2 title "D1mw transposed mult. - ways: 8"
 replot
