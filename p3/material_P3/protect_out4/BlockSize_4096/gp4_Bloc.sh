2435faeed6df76f874746e604ec7c2b669eca865
azamanis <alvaro.zamanillo@estudiante.uam.es>
2021-11-17 12:19:51 +0100

    cambios

--- p3/material_P3/protect_out4/BlockSize_4096/gp4_Bloc.sh
+++ p3/material_P3/protect_out4/BlockSize_4096/gp4_Bloc.sh
@@ -6,7 +6,7 @@ set tics font ",11"
 set key outside
 set key right top Left
 set grid
-set term pngcairo dashed size 800,400
+set term pngcairo dashed size 900,400
 set output "cache_lectura.png"
 plot "32.dat" using 1:2 with lines lt rgb "#0000FF00"  dt 1 lw 2 title "D1mr regular mult. - Block Size: 32B", \
      "32.dat" using 1:4 with lines lt rgb "#0000FF00"  dt 2 lw 2 title "D1mr transposed mult. - Block Size: 32B", \
@@ -18,7 +18,7 @@ plot "32.dat" using 1:2 with lines lt rgb "#0000FF00"  dt 1 lw 2 title "D1mr reg
      "256.dat" using 1:4 with lines lt rgb "#00FF0000"    dt 2 lw 2 title "D1mr transposed mult. - Block Size: 256B"
 replot
 
-set title "Cache write misses (4096 cache)"
+set title "Cache write misses (4096B cache)"
 set ylabel "Number of misses"
 set xlabel "Matrix size (N)" font ",10"
 set key right top Left
