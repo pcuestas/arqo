7b3c628b0eb8886e833162ce8019a7abfe73ce15
pcuestas <pablocsaa@gmail.com>
2021-11-23 16:39:16 +0100

    transpose

--- p4/material/scripts/transpose.py
+++ p4/material/scripts/transpose.py
@@ -3,15 +3,15 @@ file = "../outputs/out3/table.dat"
 A=[]
 f=open(file)
 for line in f:
-	A+=line.split("\t")
-	A[-1]=[j for j in A[-1] if j!='']
+	A+=line.split("\t\t\t\t")
 print(A)
 
-B=[[k] for k in A[0]]
 rows=len(A)
 cols=len(A[0])
-for i in range(1,rows):
+B=[([0]*rows) for j in range(cols)]
+
+for i in range(rows):
 	for j in range(cols):
-		B[j]+=[A[i][j]]
+		B[j][i]=A[i][j]
 
 print((B))
