f966c63dc56956ad84406c92caa0d00a8211ed28
pcuestas <pablocsaa@gmail.com>
2021-12-15 13:32:55 +0100

    mem

--- p4/material/src/pescalar_par2.c
+++ p4/material/src/pescalar_par2.c
@@ -38,7 +38,7 @@ int main(void)
     #pragma omp parallel for
 			for(k=0;k<M;k++)
 			{	
-				#pragma omp critical
+				#pragma omp atomic
 					sum = sum + A[k]*B[k];
 			}
 	/* Fin del computo */
